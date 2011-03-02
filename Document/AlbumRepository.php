8f052e09d190db3653a6863c2597ede8bd738161
ornicar <thibault.duplessis@gmail.com>
2011-03-01 17:27:49 -0800

    Add AlbumRepository::findAll

--- Document/AlbumRepository.php
+++ Document/AlbumRepository.php
@@ -4,11 +4,24 @@ namespace FOQ\AlbumBundle\Document;
 
 use Doctrine\ODM\MongoDB\DocumentRepository;
 use FOS\UserBundle\Model\User;
+use Zend\Paginator\Paginator;
+use ZendPaginatorAdapter\DoctrineMongoDBAdapter;
 
 class AlbumRepository extends DocumentRepository
 {
     public function findOneByUserAndSlug(User $user, $slug)
     {
         return $this->findOneBy(array('user.$id' => $user->getId(), 'slug' => $slug));
+    }
+
+    public function findAll($asPaginator = false)
+    {
+        $query = $this->createQueryBuilder()->sort('updatedAt', 'DESC');
+
+        if ($asPaginator) {
+            return new Paginator(new DoctrineMongoDBAdapter($query));
+        }
+
+        return array_values($query->getQuery()->execute()->toArray());
     }
 }
