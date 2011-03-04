a89922db8bc256eb2bc3365eb7792fc8169c7c46
ornicar <thibault.duplessis@gmail.com>
2011-03-04 14:17:30 -0800

    Replace provider reference with repository reference in uploader

--- Uploader/Uploader.php
+++ Uploader/Uploader.php
@@ -3,24 +3,24 @@
 namespace FOQ\AlbumBundle\Uploader;
 
 use FOQ\AlbumBundle\Adder\PhotoAdder;
-use FOQ\AlbumBundle\Document\PhotoProvider;
+use FOQ\AlbumBundle\Document\PhotoRepository;
 use FOQ\AlbumBundle\Model\AlbumInterface;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 class Uploader implements UploaderInterface
 {
     protected $photoAdder;
-    protected $photoProvider;
+    protected $photoRepository;
 
-    public function __construct(PhotoAdder $photoAdder, PhotoProvider $photoProvider)
+    public function __construct(PhotoAdder $photoAdder, PhotoRepository $photoRepository)
     {
-        $this->photoAdder    = $photoAdder;
-        $this->photoProvider = $photoProvider;
+        $this->photoAdder      = $photoAdder;
+        $this->photoRepository = $photoRepository;
     }
 
     public function upload(AlbumInterface $album, UploadedFile $file)
     {
-        $photo = $this->photoProvider->createPhoto();
+        $photo = $this->photoRepository->createNewPhoto();
         $photo->setFile($file);
         $this->photoAdder->add($album, $photo);
     }
