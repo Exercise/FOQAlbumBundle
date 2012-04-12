79ec4cca02b8d443f0c6ef62b569c5867736bb36
Jeremy Mikola <jmikola@gmail.com>
2012-04-11 22:38:27 -0400

    [Form] Remove arg from getDefaultOptions()
    See: symfony/symfony@b7330456b698f7dd10bd2171c7075fc34c13858e

--- Form/AlbumFormType.php
+++ Form/AlbumFormType.php
@@ -22,7 +22,7 @@ class AlbumFormType extends AbstractType
         ;
     }
 
-	public function getDefaultOptions(array $options)
+	public function getDefaultOptions()
 	{
 		return array(
 			'data_class' => $this->dataClass,
